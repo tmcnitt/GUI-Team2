a2ae8a5b0821aca050fb5332b97d8e8fc9a8bb57
Troy McNitt <tmcnitt@smu.edu>
2021-04-01 19:17:47 -0500

    Add proper promise

--- backend/jwt.js
+++ backend/jwt.js
@@ -1,44 +1,50 @@
 const pool = require("./db");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const JWT_KEY = "+EjS86shd[yAA>CA+/L*W.9'4b_r2"
+const jwt = require("jsonwebtoken");
+const JWT_KEY = "+EjS86shd[yAA>CA+/L*W.9'4b_r2";
 
 module.exports.makeJWT = (id) => {
-    return jwt.sign(id, JWT_KEY)
-}
+  return jwt.sign(id, JWT_KEY);
+};
 
 const checkJWT = (token) => {
-    let decoded = jwt.verify(token, JWT_KEY);
-    if (!decoded) {
-        return { success: false }
-    }
+  let decoded = jwt.verify(token, JWT_KEY);
+  if (!decoded) {
+    return { success: false };
+  }
 
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                return { success: false }
-            }
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        return { success: false };
+      }
 
-            connection.query("SELECT * FROM users WHERE id = ?", [decoded], (err, result) => {
-                if (err) {
-                    reject()
-                } else {
-                    resolve(result[0])
-                }
-            })
-        })
-    })
-}
+      connection.query(
+        "SELECT * FROM users WHERE id = ?",
+        [decoded],
+        (err, result) => {
+          if (err) {
+            reject();
+          } else {
+            resolve(result[0]);
+          }
+        }
+      );
+    });
+  });
+};
 
 module.exports.verifyToken = (req) => {
-    const bearerHeader = req.headers['authorization'];
+  const bearerHeader = req.headers["authorization"];
 
+  return new Promise((resolve, reject) => {
     if (bearerHeader) {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
+      const bearer = bearerHeader.split(" ");
+      const bearerToken = bearer[1];
 
-        return checkJWT(bearerToken)
+      return resolve(checkJWT(bearerToken));
     } else {
-        return { success: false }
+      return reject();
     }
-}
+  });
+};
