1c65b121c566129c5185a9a740b2a196f7155533
Troy McNitt <tmcnitt@smu.edu>
2021-04-18 20:00:51 -0500

    import sql schema

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -1,18 +1,234 @@
--- create database db
-CREATE DATABASE db;
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: excessconstructionmarketplace.c4vta9gggsb7.us-east-2.rds.amazonaws.com    Database: db
+-- ------------------------------------------------------
+-- Server version	8.0.20
 
--- use newly create database
-USE db;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
 
--- create table in db
-CREATE TABLE `db`.`test_table` (
-    `id` INT NOT NULL AUTO_INCREMENT, 
-    `value` VARCHAR(45), 
-    PRIMARY KEY (`id`), 
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
-);
+--
+-- GTID state at the beginning of the backup 
+--
 
--- insert sample entry
-INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
+--
+-- Table structure for table `auction`
+--
 
+DROP TABLE IF EXISTS `auction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auction` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `current_bid` float NOT NULL,
+  `list_user_id` bigint unsigned NOT NULL,
+  `bid_user_id` bigint unsigned NOT NULL,
+  `product_id` bigint unsigned NOT NULL,
+  `show_user_bid` tinyint(1) NOT NULL,
+  `is_finished` tinyint(1) NOT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auction.id` (`id`),
+  KEY `auction_FK` (`list_user_id`),
+  KEY `UserKey` (`bid_user_id`),
+  KEY `ProductKey` (`product_id`),
+  CONSTRAINT `ProductKey` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `bulk_listing`
+--
+
+DROP TABLE IF EXISTS `bulk_listing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bulk_listing` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint unsigned NOT NULL,
+  `list_user_id` bigint unsigned NOT NULL,
+  `is_finished` tinyint(1) NOT NULL,
+  `is_discounted` tinyint(1) NOT NULL,
+  `quantity_left` double(53,2) DEFAULT NULL,
+  `discount_end` datetime DEFAULT NULL,
+  `discount_price` double(53,2) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `price` double(53,2) NOT NULL,
+  `base_price` double(53,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `bulk_listing_id` (`product_id`),
+  KEY `bulk_listing_list_user_id` (`list_user_id`),
+  CONSTRAINT `bulk_listing_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `bulk_listing_user_id` FOREIGN KEY (`list_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `fixed_price`
+--
+
+DROP TABLE IF EXISTS `fixed_price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fixed_price` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint unsigned NOT NULL,
+  `list_user_id` bigint unsigned NOT NULL,
+  `is_finished` tinyint(1) NOT NULL,
+  `is_discounted` tinyint(1) NOT NULL,
+  `discount_end` datetime DEFAULT NULL,
+  `discount_price` double(53,2) DEFAULT NULL,
+  `description` varchar(255) NOT NULL,
+  `base_price` double(53,2) NOT NULL,
+  `quantity` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `fixed_product_id` (`product_id`),
+  KEY `fixed_list_user_id` (`list_user_id`),
+  CONSTRAINT `fixed_list_user_id` FOREIGN KEY (`list_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fixed_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `notification`
+--
+
+DROP TABLE IF EXISTS `notification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notification` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint unsigned NOT NULL,
+  `has_seen` tinyint(1) NOT NULL,
+  `date` datetime DEFAULT NULL,
+  `text` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `notification.id` (`id`),
+  KEY `notification_FK` (`user_id`),
+  CONSTRAINT `notification_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `reviewee_id` bigint unsigned NOT NULL,
+  `reviewer_id` bigint unsigned NOT NULL,
+  `review_date` date NOT NULL,
+  `msg` varchar(100) NOT NULL,
+  `stars` varchar(5) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `notification.id` (`id`),
+  KEY `review_reviewee_id` (`reviewee_id`),
+  KEY `review_reviewer_id` (`reviewer_id`),
+  CONSTRAINT `review_reviewee_id` FOREIGN KEY (`reviewee_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `review_reviewer_id` FOREIGN KEY (`reviewer_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test_table`
+--
+
+DROP TABLE IF EXISTS `test_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `test_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `value` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `transactions`
+--
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transactions` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `list_user_id` bigint unsigned NOT NULL,
+  `purchase_user_id` bigint unsigned NOT NULL,
+  `listing_type` enum('Auction','Fixed') DEFAULT NULL,
+  `quantity` int unsigned DEFAULT NULL,
+  `price` double(53,2) unsigned NOT NULL,
+  `product_id` int DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `list_user_id` (`list_user_id`),
+  KEY `purchase_user_id` (`purchase_user_id`),
+  CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`list_user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`purchase_user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `user_type` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-18 20:00:03
